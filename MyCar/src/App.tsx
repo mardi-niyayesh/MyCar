c631c527a84d0a8a09a8925ae251cc8aa681365e
niyayesh <niyayeshammardii@gmail.com>
2025-12-07 22:33:59 +0330

    refactor

--- MyCar/src/App.tsx
+++ MyCar/src/App.tsx
@@ -1,10 +1,13 @@
 import HeaderSite from "./components/Header/HeaderSite";
 import MainSite from "./components/Main/MainSite";
 function App() {
-  return <>
- <HeaderSite/>
- <MainSite/>
-  </>;
+  return (
+    <>
+      <HeaderSite/>
+      <MainSite/>
+    
+    </>
+  );
 }
 
 export default App;
