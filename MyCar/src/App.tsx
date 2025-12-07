efa61efd3a17baa5e63d3615025062e4bac563df
niyayesh <niyayeshammardii@gmail.com>
2025-12-07 18:44:26 +0330

    add section MainSite to App

--- MyCar/src/App.tsx
+++ MyCar/src/App.tsx
@@ -1,7 +1,9 @@
 import HeaderSite from "./components/Header/HeaderSite";
+import MainSite from "./components/Header/Main/MainSite";
 function App() {
   return <>
  <HeaderSite/>
+ <MainSite/>
   </>;
 }
 
