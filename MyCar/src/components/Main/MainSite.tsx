dc12a3cfe703662907c79a70496d0e169c236636
niyayesh <niyayeshammardii@gmail.com>
2025-12-07 21:22:46 +0330

    add component WhyAutoRent to parent component MainSite

---
+++ MyCar/src/components/Main/MainSite.tsx
@@ -0,0 +1,13 @@
+import CarParts from "./CarParts";
+import WhyAutoRent from "./WhyAutoRent";
+const MainSite = () => {
+    return (
+      <>
+      <CarParts/>
+      <WhyAutoRent/>
+
+      </>
+    );
+};
+
+export default MainSite;
