3057479f2b2c67bbb64654090d7d72ad380f9b89
niyayesh <niyayeshammardii@gmail.com>
2025-12-08 18:33:25 +0330

    add component footer to App

--- MyCar/src/App.tsx
+++ MyCar/src/App.tsx
@@ -1,11 +1,12 @@
 import HeaderSite from "./components/Header/HeaderSite";
 import MainSite from "./components/Main/MainSite";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
       <HeaderSite/>
       <MainSite/>
-    
+      <Footer/>
     </>
   );
 }
