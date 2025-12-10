f49ad1e6e42f089565ac443c7e66056f0f3b5f1a
niyayesh <niyayeshammardii@gmail.com>
2025-12-10 19:40:37 +0330

    add slider to MainSite

--- MyCar/src/components/Main/MainSite.tsx
+++ MyCar/src/components/Main/MainSite.tsx
@@ -1,11 +1,12 @@
 import CarParts from "./CarParts";
 import WhyAutoRent from "./WhyAutoRent";
+import Slider from "./Slider";
 const MainSite = () => {
   return (
     <>
       <CarParts/>
       <WhyAutoRent/>
-  
+      <Slider/>
     </>
   );
 };
