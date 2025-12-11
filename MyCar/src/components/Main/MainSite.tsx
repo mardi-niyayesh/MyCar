4d288e8a0865e3e9306787d1db1c093b41ca412b
niyayesh <niyayeshammardii@gmail.com>
2025-12-11 16:46:01 +0330

    add component ReserveAutorent to MinSite

--- MyCar/src/components/Main/MainSite.tsx
+++ MyCar/src/components/Main/MainSite.tsx
@@ -1,12 +1,14 @@
 import CarParts from "./CarParts";
 import WhyAutoRent from "./WhyAutoRent";
 import Slider from "./Slider";
+import ReserveAutorent from "./ReserveAutorent";
 const MainSite = () => {
   return (
     <>
-      <CarParts/>
-      <WhyAutoRent/>
-      <Slider/>
+      <CarParts />
+      <WhyAutoRent />
+      <Slider />
+      <ReserveAutorent />
     </>
   );
 };
