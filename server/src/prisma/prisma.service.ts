d96dc0a94a491e5e7d3cbef1656d7b3efcb65ba8
homow <homowsh897@gmail.com>
2026-02-07 05:35:02 +0330

    refactor(style)

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -4,33 +4,35 @@ import {Injectable, OnModuleInit, OnModuleDestroy} from '@nestjs/common';
 
 @Injectable()
 export class PrismaService
-    extends PrismaClient
-    implements OnModuleInit, OnModuleDestroy {
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
 
-    private static initialize: boolean= false;
+  private static initialize: boolean = false;
 
-    constructor() {
-        const adapter = new PrismaPg({
-            connectionString: process.env.DATABASE_URL as string,
-        });
-        super({
-            adapter,
-            errorFormat: 'minimal',
-            log: ['info', 'error', 'warn'],
-        });
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({
+      adapter,
+      errorFormat: 'minimal',
+      log: ['info', 'error', 'warn'],
+    });
+  }
+
+  // noinspection JSUnusedGlobalSymbols
+  async onModuleInit() {
+    if (!PrismaService.initialize) {
+      PrismaService.initialize = true;
+      await this.$connect();
     }
+  }
 
-    async onModuleInit() {
-        if (!PrismaService.initialize) {
-            PrismaService.initialize = true;
-            await this.$connect();
-        }
+  // noinspection JSUnusedGlobalSymbols
+  async onModuleDestroy() {
+    if (!PrismaService.initialize) {
+      PrismaService.initialize = false;
+      await this.$disconnect();
     }
-
-    async onModuleDestroy() {
-        if (!PrismaService.initialize) {
-            PrismaService.initialize = false;
-            await this.$disconnect();
-        }
-    }
+  }
 }
