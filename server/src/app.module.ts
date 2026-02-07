1c18a5cc35b097060db0ba93ec60c7125aee8bc6
homow <homowsh897@gmail.com>
2026-02-08 03:25:19 +0330

    feat(module-file) users

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,8 @@
 import {Module} from '@nestjs/common';
 import {PrismaModule} from "./modules/prisma";
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule]
+  imports: [PrismaModule, UsersModule]
 })
 export class AppModule {}
