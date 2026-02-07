bf8b5566e13d313df501cb6d40a63b20692cc036
homow <homowsh897@gmail.com>
2026-02-07 04:33:02 +0330

    feat(validation) set in main

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import {AppModule} from './app.module';
 import {NestFactory} from '@nestjs/core';
+import {ValidationPipe} from "@nestjs/common";
 
+/** run application */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /** global configs */
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
