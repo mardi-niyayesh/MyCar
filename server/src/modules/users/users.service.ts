2ffc4b126b636e6547641bb0f6a8c9d2110a1720
homow <homowsh897@gmail.com>
2026-02-08 05:39:42 +0330

    feat(service) create user services

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,4 +1,28 @@
 import {Injectable} from '@nestjs/common';
+import {CreateUserInput} from "./validators";
+import {BaseApiResponseType} from "../../lib";
+import {User} from "../prisma/generated/client";
+import {UserRole} from "../prisma/generated/enums";
+import {PrismaService} from "../prisma/prisma.service";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  /** create user in db */
+  async create(createData: CreateUserInput): Promise<BaseApiResponseType<{ user: User }>> {
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...createData,
+        role: UserRole.USER
+      }
+    });
+
+    return {
+      message: "user created successfully",
+      data: {
+        user: newUser
+      }
+    };
+  }
+}
